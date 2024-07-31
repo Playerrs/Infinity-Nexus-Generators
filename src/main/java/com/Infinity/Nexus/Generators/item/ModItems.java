fae2f49b2f0a60a7b0448848e40db6b57a50868b
Player_rs <josueplayerrs@gmail.com>
2024-07-30 22:46:55 -0300

    + Scanner, precisa fazer uma logica boa

--- src/main/java/com/Infinity/Nexus/Generators/item/ModItems.java
+++ src/main/java/com/Infinity/Nexus/Generators/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.Infinity.Nexus.Generators.item;
 
 import com.Infinity.Nexus.Generators.InfinityNexusGenerators;
+import com.Infinity.Nexus.Generators.item.custom.CrudeOilScannerItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,7 +13,10 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, InfinityNexusGenerators.MOD_ID);
 
     public static final RegistryObject<Item> DRILL = ITEMS.register("drill",
-            () -> new Item(new Item.Properties().durability(500)));
+            () -> new Item(new Item.Properties().durability(512)));
+
+    public static final RegistryObject<Item> CRUDE_OIL_SCANNER = ITEMS.register("crude_oil_scanner",
+            () -> new CrudeOilScannerItem(new Item.Properties().durability(256)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/com/Infinity/Nexus/Generators/item/custom/CrudeOilScannerItem.java
@@ -0,0 +1,40 @@
+package com.Infinity.Nexus.Generators.item.custom;
+
+import com.Infinity.Nexus.Generators.InfinityNexusGenerators;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.LevelChunkSection;
+
+import java.util.Random;
+
+public class CrudeOilScannerItem extends Item {
+    public CrudeOilScannerItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        ItemStack itemStackInHand = pPlayer.getItemInHand(pUsedHand);
+
+        if(!pLevel.isClientSide()) {
+            int chance = new Random().nextInt(100);
+
+            ChunkAccess chunk = pLevel.getChunkAt(pPlayer.blockPosition());
+            pPlayer.sendSystemMessage(Component.literal(String.valueOf(chunk.getStatus().getIndex()))); // pq da sempre 11?
+
+
+            pPlayer.sendSystemMessage(Component.literal(chance + "% chance"));
+        }
+
+        pPlayer.getCooldowns().addCooldown(this, 20);                                // Adiciona cooldown
+        itemStackInHand.hurtAndBreak(1, pPlayer, pPlayer1 -> pPlayer1.broadcastBreakEvent(pUsedHand)); // Tira durabilidade
+
+        return InteractionResultHolder.sidedSuccess(itemStackInHand, pLevel.isClientSide());
+    }
+}
